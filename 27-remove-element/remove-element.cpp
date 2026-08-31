e0fd4028548fb5b5cf67b6ec5ddd3edd1c2e77e5
Ashutosh Srivastava <ashuroxkz5010@gmail.com>
2026-09-01 01:05:02 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (12.45%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -3,6 +3,8 @@ public:
     int removeElement(vector<int>& nums, int val) {
         int k = 0;
 
+        //self k swap
+
         for (int i = 0; i < nums.size(); i++) {
             if (nums[i] != val) {
                 swap(nums[i], nums[k]);
