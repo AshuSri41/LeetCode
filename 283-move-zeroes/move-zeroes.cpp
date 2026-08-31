48275449f11c09638721aaef54210e76f5e3358f
Ashutosh Srivastava <ashuroxkz5010@gmail.com>
2026-09-01 04:04:10 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (82.82%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int left = 0;
-
+            // neetcode approach left and right pointer swap
         for (int right = 0; right < nums.size(); right++) {
             if (nums[right] != 0) {
                 swap(nums[left], nums[right]);
