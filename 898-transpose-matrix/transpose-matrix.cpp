1d78d1a59237af59d4103a54e6952df68883421c
Ashutosh Srivastava <ashuroxkz5010@gmail.com>
2026-09-01 18:15:03 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (34.66%) - LeetSync

---
+++ 898-transpose-matrix/transpose-matrix.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        //diagonal swap app
+        if (n != m) {
+            vector<vector<int>> ans(m, vector<int>(n));
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    ans[j][i] = matrix[i][j];
+                }
+            }
+
+            return ans;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+
+        return matrix;
+    }
+};
