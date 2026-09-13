90c6531c968211c2ddc43423020495ee72ffcfbe
Ashutosh Srivastava <ashuroxkz5010@gmail.com>
2026-09-13 22:49:51 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (96.60%) - LeetSync

---
+++ 67-add-binary/add-binary.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string s;
+        int i=a.size()-1,j=b.size()-1,c=0;
+        while(i>=0||j>=0||c){
+            int x=c;
+            if(i>=0)x+=a[i--]-'0';
+            if(j>=0)x+=b[j--]-'0';
+            s+=x%2+'0';
+            c=x/2;
+        }
+        reverse(s.begin(),s.end());
+        return s;
+    }
+};
