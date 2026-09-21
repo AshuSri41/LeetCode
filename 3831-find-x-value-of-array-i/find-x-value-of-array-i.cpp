dc1087e4ed1e588d92f6541a22ee4ae92bee5700
Ashutosh Srivastava <ashuroxkz5010@gmail.com>
2026-09-21 22:38:54 +0530

    Time: 164 ms (47.32%) | Memory: 151.2 MB (55.36%) - LeetSync

---
+++ 3831-find-x-value-of-array-i/find-x-value-of-array-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<long long> resultArray(vector<int>& nums, int k) {
+        vector<long long> ans(k), dp(k);
+
+        for(int num : nums) {
+            vector<long long> cur(k);
+            int x = num % k;
+
+            cur[x]++;
+
+            for(int r = 0; r < k; r++)
+                cur[(r * x) % k] += dp[r];
+
+            dp = cur;
+
+            for(int r = 0; r < k; r++)
+                ans[r] += dp[r];
+        }
+
+        return ans;
+    }
+};
